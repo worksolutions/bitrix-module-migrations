09e6d1259fc61f491c88ffe5b34605c30920ade1
Maxim Sokolovsky <sokolovsky@worksolutions.ru>
2014-06-02 21:53:08 +0400

    Общий процесс учета фиксации изменений.  Определение базового обработчика предметов изменений.

---
+++ lib/SubjectHandler.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Maxim Sokolovsky <sokolovsky@worksolutions.ru>
+ */
+
+namespace WS\Migrations;
+
+
+abstract class SubjectHandler {
+    static public function className() {
+        return get_called_class();
+    }
+
+    /**
+     * @return Localization
+     */
+    protected function getLocalization() {
+        return Module::getInstance()->getLocalization('handlers');
+    }
+
+    /**
+     * Name of Handler in Web interface
+     * @return string
+     */
+    abstract public function getName();
+
+    abstract public function getIdByChangeMethod($method, $data = array());
+
+    abstract public function getSnapshot($id);
+
+    abstract public function applySnapshot($data);
+
+    public function analysisOfChanges($updatedData, $baseData = null) {
+        return $updatedData;
+    }
+
+    public function applyChanges($data) {
+        $this->applySnapshot($data);
+    }
+}
