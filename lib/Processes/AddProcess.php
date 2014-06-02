37fa2c221c389dee3b18dd80a37b251cfa610021
Maxim Sokolovsky <sokolovsky@worksolutions.ru>
2014-06-02 21:58:56 +0400

    Общий процесс учета фиксации изменений.  Определение основных процессов миграций.

---
+++ lib/Processes/AddProcess.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Maxim Sokolovsky <sokolovsky@worksolutions.ru>
+ */
+
+namespace WS\Migrations\Processes;
+
+
+class AddProcess extends BaseProcess {
+
+    public function update($log) {
+    }
+
+    public function rollback($log) {
+    }
+}
