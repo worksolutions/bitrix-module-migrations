0e0b480152fcc6d73d816391172545f1cc3b480b
Maxim Sokolovsky <sokolovsky@worksolutions.ru>
2014-06-05 12:43:39 +0400

    Применение миграций. Внедрение таблицы установок.

--- install/db/install.sql
+++ install/db/install.sql
@@ -9,3 +9,8 @@ CREATE TABLE IF NOT EXISTS `ws_migrations_apply_changes_log` (
   `DESCRIPTION` TEXT NOT NULL ,
   INDEX (`type`)
 );
+CREATE TABLE IF NOT EXISTS `ws_migrations_setups_log` (
+  `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `USER_ID` INT,
+  `DATE` DATETIME NOT NULL
+);
--- install/db/uninstall.sql
+++ install/db/uninstall.sql
@@ -1,1 +1,2 @@
 DROP TABLE if exists ws_migrations_apply_changes_log;
+DROP TABLE if exists ws_migrations_setups_log;
