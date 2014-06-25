018048679f8cce8f3c01ec1d7e70ca385b896712
Maxim Sokolovsky <sokolovsky@worksolutions.ru>
2014-06-25 12:59:37 +0400

    Пункты меню. Внедрение ссылок импорта/экспорта.

--- admin/menu.php
+++ admin/menu.php
@@ -3,16 +3,30 @@ global $USER;
 if (!$USER->isAdmin()) {
     return array();
 }
+$loc = \WS\Migrations\Module::getInstance()->getLocalization('menu');
 $inputUri = '/bitrix/admin/ws_migrations.php?q=';
 return array(
     array(
         'parent_menu' => 'global_menu_settings',
         'sort' => 500,
-        'text' => 'Миграции данных',
-        'title' => 'Миграции данных',
+        'text' => $loc->getDataByPath('title'),
+        'title' => $loc->getDataByPath('title'),
         'module_id' => 'ws.migrations',
         'icon' => '',
         'items_id' => 'ws_migrations_menu',
-        'url' => $inputUri.'main'
+        'items' => array(
+            array(
+                'text' => $loc->getDataByPath('apply'),
+                'url' => $inputUri.'main',
+            ),
+            array(
+                'text' => $loc->getDataByPath('import'),
+                'url' => $inputUri.'import',
+            ),
+            array(
+                'text' => $loc->getDataByPath('export'),
+                'url' => $inputUri.'export',
+            ),
+        )
     )
 );
