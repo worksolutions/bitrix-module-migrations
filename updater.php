3b92bc4b8a04219a949e8dbb8cce312790a21cc3
Максим Соколовский <sokolovsky@worksolutions.ru>
2015-12-29 12:49:43 +0300

    Файл установки. Очистка

--- updater.php
+++ updater.php
@@ -20,37 +20,3 @@ $modulePath = rtrim($docRoot, '/').$updater->kernelPath.'/modules/'.$updater->mo
 $updatePath = $docRoot.$updater->curModulePath;
 
 $isInstalled = \Bitrix\Main\ModuleManager::isModuleInstalled($updater->moduleID);
-if (!$isInstalled) {
-    return;
-}
-
-if (!is_dir($uploadDir.'/ws.migrations')) {
-    $copyRes = CopyDirFiles($updatePath.'/install/upload', $uploadDir, false, true);
-    if (!$copyRes) {
-        $fAddErrorMessage("Use platform version catalog not setup");
-        return;
-    }
-    // init file version
-    $versionRaw = \COption::GetOptionString($updater->moduleID, 'version');
-    $version = unserialize($versionRaw);
-
-    $ownerRaw = \COption::GetOptionString($updater->moduleID, 'owner');
-    $owner = unserialize($ownerRaw);
-
-    $createVersionFile = file_put_contents(
-        $uploadDir.'/ws.migrations/version.dat',
-        $version.':#:'.md5($version.$modulePath.'/lib/platformversion.php').':#:'.$owner
-    );
-    if (!$createVersionFile) {
-        $fAddErrorMessage("Version file was not created");
-        return;
-    }
-}
-
-// cli
-$dest = $docRoot.'bitrix/tools';
-$copyRes = CopyDirFiles($docRoot .$updater->curModulePath.'/install/tools', $dest, false, true);
-if (!$copyRes) {
-    $fAddErrorMessage("Cli interface file was not created");
-    return;
-}
